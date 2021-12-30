c17767128b771821c1875cd8b0e77b4076970fde
MustafaNajm <mustfaamohammed@gmail.com>
2021-12-30 17:00:44 +0300

    My first commit

---
+++ UserService/Database/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Database.Entities;
+
+namespace UserService.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=LAPTOP-BRPSLIQQ; Initial Catalog=master;Persist Security Info=True;User ID=As;password=123must;");
+        }
+    }
+}
