bfb80407c3132166d7267cc20029e318cd241ba2
MustafaNajm <96866509+MustafaNajm@users.noreply.github.com>
2022-01-02 14:34:05 +0300

    Update Startup.cs

--- UserService/Startup.cs
+++ UserService/Startup.cs
@@ -25,6 +25,7 @@ namespace UserService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<IDatabaseContext, db>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
